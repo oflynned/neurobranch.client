59b10a1ecc411e778fc5e38e4f790f965bdf495f
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-14 19:44:17 +0200

    chore: move provider auth

--- apps/researcher-client/providers/auth/use-auth.hook.tsx
+++ apps/researcher-client/providers/auth/use-auth.hook.tsx
@@ -1,41 +1,52 @@
 import React, { useState, useEffect, useContext, createContext } from 'react';
 import nookies from 'nookies';
 import { FirebaseUser, firebaseClient } from './firebase.client';
+import { log } from 'util';
 
 const AuthContext = createContext<{
   user: FirebaseUser | null;
   token: string | null;
   isAuthenticated: boolean;
+  isLoading: boolean;
 }>({
   user: null,
   token: null,
   isAuthenticated: false,
+  isLoading: true,
 });
 
-export function AuthProvider({ children }: never) {
+export function AuthProvider({ children }) {
   const [user, setUser] = useState<FirebaseUser | null>(null);
   const [token, setToken] = useState<string | null>(null);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const logout = async (): Promise<void> => {
+    setUser(null);
+    setIsAuthenticated(false);
+    setIsLoading(false);
+    nookies.destroy(null, 'token');
+    nookies.set(null, 'token', '', { path: '/' });
+  };
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       (window as any).nookies = nookies;
     }
 
+    setIsLoading(true);
+
     return firebaseClient.auth().onIdTokenChanged(async (user) => {
       if (!user) {
-        setUser(null);
-        nookies.destroy(null, 'token');
-        nookies.set(null, 'token', '', { path: '/' });
+        await logout();
         return;
       }
 
       const token = await user.getIdToken();
-
       setUser(user);
       setToken(token);
-
-      setIsAuthenticated(!!user && !!token);
+      setIsAuthenticated(true);
+      setIsLoading(false);
 
       nookies.destroy(null, 'token');
       nookies.set(null, 'token', token, { path: '/' });
@@ -45,21 +56,28 @@ export function AuthProvider({ children }: never) {
   // force refresh the token every 10 minutes
   useEffect(() => {
     const handle = setInterval(async () => {
-      const user = firebaseClient.auth().currentUser;
+      setIsLoading(true);
 
-      if (user) {
-        const token = await user.getIdToken(true);
-        setUser(user);
+      const currentUser = firebaseClient.auth().currentUser;
+
+      if (currentUser) {
+        const token = await currentUser.getIdToken(true);
+        setUser(currentUser);
         setToken(token);
+        setIsAuthenticated(true);
+      } else {
+        setUser(null);
+        setToken(null);
+        setIsAuthenticated(false);
       }
 
-      setIsAuthenticated(!!user && !!token);
+      setIsLoading(false);
     }, 10 * 60 * 1000);
     return () => clearInterval(handle);
-  }, [token]);
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, token, isAuthenticated }}>
+    <AuthContext.Provider value={{ user, token, isAuthenticated, isLoading }}>
       {children}
     </AuthContext.Provider>
   );
