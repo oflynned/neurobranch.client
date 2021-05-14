b1204470e7b343cd68e41e18dfdcb1db52f552c7
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-14 19:45:50 +0200

    chore: add login styling

--- apps/researcher-client/pages/index.tsx
+++ apps/researcher-client/pages/index.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import Link from 'next/link';
-import { useAuth, firebaseClient } from '../auth';
+import { useAuth, firebaseClient } from '../providers/auth';
 
 export function Index() {
-  const { user, token, isAuthenticated } = useAuth();
+  const { user, token, isAuthenticated, isLoading } = useAuth();
 
-  if (isAuthenticated) {
+  if (isLoading) {
+    return <h1>LOADING</h1>;
+  }
+
+  if (!isAuthenticated) {
     return (
       <div>
-        <p>Authenticated!</p>
-        <button onClick={() => firebaseClient.auth().signOut()}>Log out</button>
-
-        <div>
-          <p>Uid</p>
-          <code>{user.uid}</code>
-          <p>Base64 encoded</p>
-          <code>{Buffer.from(user.uid).toString('base64')}</code>
-        </div>
-        <div>
-          <p>Refresh token</p>
-          <code>{token}</code>
-          <p>Base64 encoded</p>
-          <code>{Buffer.from(token).toString('base64')}</code>
-        </div>
+        <p>Please login</p>
+        <Link href={'/login'}>Login</Link>
       </div>
     );
   }
 
   return (
     <div>
-      <p>User id n/a</p>
-      <Link href={'/login'}>Login</Link>
+      <p>Authenticated!</p>
+      <button onClick={async () => firebaseClient.auth().signOut()}>
+        Log out
+      </button>
+
+      <div>
+        <p>Uid</p>
+        <code>{user.uid}</code>
+        <p>Base64 encoded</p>
+        <code>{Buffer.from(user.uid).toString('base64')}</code>
+      </div>
+      <div>
+        <p>Refresh token</p>
+        <code>{token}</code>
+        <p>Base64 encoded</p>
+        <code>{Buffer.from(token).toString('base64')}</code>
+      </div>
     </div>
   );
 }
