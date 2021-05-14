1bc4f5a1798fb8fcb695f3a9914c864a133d9856
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-14 19:41:15 +0200

    chore: update next config to use next images w/ plugins helper

--- apps/researcher-client/next.config.js
+++ apps/researcher-client/next.config.js
@@ -1,10 +1,26 @@
+const path = require('path');
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withImages = require('next-images');
 
-module.exports = withNx({
-  nx: {
-    // Set this to false if you do not want to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: true,
+const withPlugins = require('next-compose-plugins');
+
+module.exports = {
+  sassOptions: {
+    includedPaths: [path.join(__dirname, 'styles')],
   },
-});
+  ...withPlugins([
+    [
+      withNx,
+      {
+        nx: {
+          // Set this to false if you do not want to use SVGR
+          // See: https://github.com/gregberge/svgr
+          svgr: true,
+        },
+      },
+    ],
+    withImages,
+  ]),
+};
