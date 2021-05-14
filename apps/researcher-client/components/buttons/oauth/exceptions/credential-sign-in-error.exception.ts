11b03c04beb74ce9be09d9393830b763120fa156
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-14 19:41:40 +0200

    chore: add base exceptions

---
+++ apps/researcher-client/components/buttons/oauth/exceptions/credential-sign-in-error.exception.ts
@@ -0,0 +1,1 @@
+export class CredentialSignInErrorException extends Error {}
---
+++ apps/researcher-client/components/buttons/oauth/exceptions/index.ts
@@ -0,0 +1,3 @@
+export { EmailTooShortException } from './email-too-short.exception';
+export { PasswordTooShortException } from './password-too-short.exception';
+export { CredentialSignInErrorException } from './credential-sign-in-error.exception';
