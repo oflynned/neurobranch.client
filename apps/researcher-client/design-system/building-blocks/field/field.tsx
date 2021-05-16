fe35ca964133489f60c968bb3141414044839a4b
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-16 18:47:38 +0200

    chore: add error state to field

--- apps/researcher-client/design-system/building-blocks/field/field.tsx
+++ apps/researcher-client/design-system/building-blocks/field/field.tsx
@@ -8,6 +8,8 @@ interface Props {
   type?: 'password' | 'text';
   hint: string;
   label?: string;
+  hasError?: boolean;
+  error?: string;
   onTextEntered?: (text) => Promise<void> | void;
 }
 
@@ -16,6 +18,8 @@ export const Field: FC<Props> = ({
   hint,
   label,
   onTextEntered,
+  error,
+  hasError = false,
 }) => {
   const [id] = useState<string>(v4());
   const [isActive, setIsActive] = useState<boolean>(true);
@@ -24,7 +28,7 @@ export const Field: FC<Props> = ({
   return (
     <div className={styles.field}>
       {label && (
-        <label htmlFor={id}>
+        <label htmlFor={id} className={hasError && styles.error}>
           <Paragraph>{label}</Paragraph>
         </label>
       )}
@@ -34,12 +38,17 @@ export const Field: FC<Props> = ({
         placeholder={hint}
         onFocus={() => setIsActive(true)}
         onBlur={() => setIsActive(false)}
-        className={`${styles.inputBox} ${isActive && styles.active}`}
+        className={`${isActive && styles.active} ${hasError && styles.error}`}
         onChange={(event) => {
           setValue(event.target.value);
           onTextEntered(value);
         }}
       />
+      {hasError && (
+        <div className={styles.error}>
+          <Paragraph>{error}</Paragraph>
+        </div>
+      )}
     </div>
   );
 };
