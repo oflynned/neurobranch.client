157bf215a77465ddfeaf5fecf71e7cde2b8b1ab1
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-17 18:54:43 +0200

    chore: add route guard

---
+++ apps/researcher-client/design-system/components/route/route.guard.tsx
@@ -0,0 +1,42 @@
+import { useFirebase } from '../../../providers/auth';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { Paragraph } from '../../building-blocks';
+
+const unprotectedRoutes = ['/login', '/register', '/forgot-password'];
+
+const isPathProtected = (path: string): boolean =>
+  !unprotectedRoutes.includes(path);
+
+export const RouteGuard = ({ children }) => {
+  const router = useRouter();
+  const { user, token, isLoading } = useFirebase();
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    const isAuthenticated = !!user && !!token;
+
+    if (router.pathname === '/') {
+      if (isAuthenticated) {
+        router.push('/dashboard');
+      } else {
+        router.push('/login');
+      }
+    }
+
+    if (!isAuthenticated && isPathProtected(router.pathname)) {
+      router.push('/login');
+    }
+  }, [isLoading, router, token, user]);
+
+  console.log({ user, token, isLoading });
+
+  if (isLoading) {
+    return <Paragraph>Loading...</Paragraph>;
+  }
+
+  return children;
+};
