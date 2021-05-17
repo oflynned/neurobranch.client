686d3f1e473a73909063cf7df3148b9edd4877cf
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-17 18:51:42 +0200

    chore: add naive attempt at useAccount w/o using cookies or local storage

--- apps/researcher-client/pages/dashboard/index.tsx
+++ apps/researcher-client/pages/dashboard/index.tsx
@@ -1,17 +1,35 @@
-import { Title } from '../../design-system';
-import { NavBar } from '../../design-system/components/nav-bar';
-
+import { Title, NavBar, Button, Paragraph } from '../../design-system';
 import styles from './style.module.scss';
+import { firebaseClient } from '../../providers/auth/firebase.client';
+import React, { FC } from 'react';
+import { useAccount } from '../../providers/auth/use-account.hook';
+
+const Layout: FC = ({ children }) => {
+  return <main className={styles.content}>{children}</main>;
+};
 
 const Index = () => {
+  const { account } = useAccount();
+
   return (
     <div className={styles.page}>
       <div className={styles.nav}>
         <NavBar activePage={'HOME'} />
       </div>
-      <section className={styles.content}>
-        <Title>Dashboard</Title>
-      </section>
+      <Layout>
+        <Title>Authenticated!</Title>
+        <div>
+          <Button
+            text={'Log out'}
+            onClick={async () => {
+              await firebaseClient.auth().signOut();
+              window.location.href = '/';
+            }}
+          />
+
+          {account && <Paragraph>Hey there, {account.name}!</Paragraph>}
+        </div>
+      </Layout>
     </div>
   );
 };
