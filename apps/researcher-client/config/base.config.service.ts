7c28eb33e0de329a841a892ba9e0fe08edccc304
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-21 13:56:27 +0200

    chore: add config service

---
+++ apps/researcher-client/config/base.config.service.ts
@@ -0,0 +1,33 @@
+export type Optional<T> = T | undefined | null;
+
+export class BaseConfigService {
+  constructor(private readonly env: Record<string, string>) {}
+
+  protected getValue(key: string, throwOnMissing = false): Optional<string> {
+    const value = this.env[key];
+
+    if (!value && throwOnMissing) {
+      throw new Error(`config error - missing env.${key}`);
+    }
+
+    return value ?? null;
+  }
+
+  protected ensureValues(keys: string[]): BaseConfigService {
+    keys.forEach((key) => this.getValue(key));
+
+    return this;
+  }
+
+  isProduction(): boolean {
+    return this.getValue('NODE_ENV') === 'production';
+  }
+
+  isDevelopment(): boolean {
+    return this.getValue('NODE_ENV') === 'development';
+  }
+
+  isTest(): boolean {
+    return this.getValue('NODE_ENV') === 'test';
+  }
+}
