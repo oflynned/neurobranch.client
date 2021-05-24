1c77610805ec65dcc855936dbc99abffa250823f
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-24 19:14:20 +0200

    feat: add disabled and text prefills

--- apps/investigator/design-system/building-blocks/field/field.tsx
+++ apps/investigator/design-system/building-blocks/field/field.tsx
@@ -6,25 +6,29 @@ import styles from './style.module.scss';
 
 interface Props {
   type?: 'password' | 'text';
-  hint: string;
+  hint?: string;
   label?: string;
   showError?: boolean;
   error?: string;
   onTextEntered?: (text: string) => Promise<void> | void;
   onError?: (error: Error) => Promise<void> | void;
+  disabled?: boolean;
+  text?: string;
 }
 
 export const Field: FC<Props> = ({
   type = 'text',
-  hint,
+  hint = '',
   label,
   onTextEntered,
   error,
   showError = false,
+  disabled = false,
+  text,
 }) => {
   const [id] = useState<string>(v4());
   const [isActive, setIsActive] = useState<boolean>(true);
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>(text ?? '');
 
   return (
     <div className={styles.field}>
@@ -42,9 +46,14 @@ export const Field: FC<Props> = ({
         className={`${isActive ? styles.active : undefined} ${
           showError ? styles.error : undefined
         }`}
+        disabled={disabled}
+        value={value}
         onChange={(event) => {
           setValue(event.target.value);
-          onTextEntered(value);
+
+          if (onTextEntered) {
+            onTextEntered(value);
+          }
         }}
       />
       {showError && (
