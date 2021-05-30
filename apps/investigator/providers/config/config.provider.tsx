d402449810c98c5e3dbe3d37d98b72cadd30a934
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-30 18:32:02 +0200

    chore: create config provider

---
+++ apps/investigator/providers/config/config.provider.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useContext, createContext, useEffect } from 'react';
+import { ConfigService } from '../../config/config.service';
+
+const ConfigContext = createContext<{
+  config: ConfigService | null;
+}>({
+  config: null,
+});
+
+export const ConfigProvider = ({ children }) => {
+  const [config] = useState(new ConfigService());
+
+  return (
+    <ConfigContext.Provider value={{ config }}>
+      {children}
+    </ConfigContext.Provider>
+  );
+};
+
+export const useConfig = () => {
+  return useContext(ConfigContext);
+};
