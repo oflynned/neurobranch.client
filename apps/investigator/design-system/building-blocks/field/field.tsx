32781be4175a39957cad6c0d932465de07d64664
Edmond Ó Floinn <oflynned@tcd.ie>
2021-06-13 20:04:09 +0200

    chore: add disabled and initial values of field

--- apps/investigator/design-system/building-blocks/field/field.tsx
+++ apps/investigator/design-system/building-blocks/field/field.tsx
@@ -1,13 +1,14 @@
 import { FC, useState } from 'react';
 import { v4 } from 'uuid';
 import { Paragraph } from '../text';
-
 import styles from './style.module.scss';
 
 interface Props {
   type?: 'password' | 'text';
   hint: string;
+  initialValue?: string;
   label?: string;
+  disabled?: boolean;
   showError?: boolean;
   error?: string;
   onTextEntered?: (text: string) => Promise<void> | void;
@@ -16,15 +17,17 @@ interface Props {
 
 export const Field: FC<Props> = ({
   type = 'text',
+  initialValue = '',
   hint,
   label,
   onTextEntered,
   error,
   showError = false,
+  disabled = false,
 }) => {
   const [id] = useState<string>(v4());
   const [isActive, setIsActive] = useState<boolean>(true);
-  const [value, setValue] = useState<string>('');
+  const [fieldValue, setFieldValue] = useState<string>(initialValue);
 
   return (
     <div className={styles.field}>
@@ -36,15 +39,17 @@ export const Field: FC<Props> = ({
       <input
         id={id}
         type={type}
+        value={fieldValue}
         placeholder={hint}
         onFocus={() => setIsActive(true)}
         onBlur={() => setIsActive(false)}
         className={`${isActive ? styles.active : undefined} ${
           showError ? styles.error : undefined
         }`}
+        disabled={disabled}
         onChange={(event) => {
-          setValue(event.target.value);
-          onTextEntered(value);
+          setFieldValue(event.target.value);
+          onTextEntered && onTextEntered(initialValue);
         }}
       />
       {showError && (
