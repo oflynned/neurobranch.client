7aa0fd374dfd348087353a2211b6c90e6e7d189a
Edmond Ó Floinn <oflynned@tcd.ie>
2021-06-19 20:10:13 +0200

    chore: update config

--- apps/investigator/next.config.js
+++ apps/investigator/next.config.js
@@ -1,25 +1,69 @@
+const fs = require('fs');
 const path = require('path');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPlugins = require('next-compose-plugins');
 const withNx = require('@nrwl/next/plugins/with-nx');
 const withImages = require('next-images');
-const withPlugins = require('next-compose-plugins');
 
-module.exports = {
+const withLess = require('@zeit/next-less');
+const withSass = require('@zeit/next-sass');
+const lessToJS = require('less-vars-to-js');
+
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/theme.less'), 'utf8'),
+);
+
+const plugins = [
+  [withNx, { nx: { svgr: true } }],
+  [withImages],
+  [
+    withLess,
+    {
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables,
+      },
+      webpack: (config, { isServer }) => {
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          config.externals = [
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === 'function') {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+          ];
+
+          config.module.rules.unshift({
+            test: antStyles,
+            use: 'null-loader',
+          });
+        }
+        return config;
+      },
+    },
+  ],
+  [
+    withSass,
+    {
+      cssModules: true,
+      cssLoaderOptions: {
+        localIdentName: '[path]___[local]___[hash:base64:5]',
+      },
+    },
+  ],
+];
+
+const nextConfig = {
   sassOptions: {
     includedPaths: [path.join(__dirname, 'styles')],
   },
-  ...withPlugins([
-    [
-      withNx,
-      {
-        nx: {
-          // Set this to false if you do not want to use SVGR
-          // See: https://github.com/gregberge/svgr
-          svgr: true,
-        },
-      },
-    ],
-    withImages,
-  ]),
 };
+
+module.exports = withPlugins(plugins, nextConfig);
