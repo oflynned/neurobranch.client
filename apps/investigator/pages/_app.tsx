ed6919928928331b998e12b91973f9a428120aac
Edmond Ó Floinn <oflynned@tcd.ie>
2021-06-27 15:25:29 +0200

    chore: use theme in app

--- apps/investigator/pages/_app.tsx
+++ apps/investigator/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
+import 'tailwindcss/tailwind.css';
 import { ConfigService } from '../config/config.service';
 import { RouteGuard } from '../design-system';
 import { FirebaseProvider } from '../providers/auth';
 import { AccountProvider } from '../providers/auth/use-account.hook';
 import { ConfigProvider } from '../providers/config/config.provider';
-import './app.scss';
-import './reset.scss';
+import { GlobalStyle } from '../theme/global.theme';
+import { lightTheme } from '../theme/light.theme';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const config = new ConfigService();
@@ -19,16 +21,19 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Reset />
+      <GlobalStyle />
       <ConfigProvider>
-        <ApolloProvider client={client}>
-          <FirebaseProvider>
-            <AccountProvider>
-              <RouteGuard>
-                <Component {...pageProps} />
-              </RouteGuard>
-            </AccountProvider>
-          </FirebaseProvider>
-        </ApolloProvider>
+        <ThemeProvider theme={lightTheme}>
+          <ApolloProvider client={client}>
+            <FirebaseProvider>
+              <AccountProvider>
+                <RouteGuard>
+                  <Component {...pageProps} />
+                </RouteGuard>
+              </AccountProvider>
+            </FirebaseProvider>
+          </ApolloProvider>
+        </ThemeProvider>
       </ConfigProvider>
     </>
   );
