73fadeb2463fc0384550debded466636050f3e9a
Edmond Ó Floinn <oflynned@tcd.ie>
2021-06-27 17:10:32 +0200

    chore: add padding to card

--- apps/investigator/design-system/building-blocks/card/card.tsx
+++ apps/investigator/design-system/building-blocks/card/card.tsx
@@ -1,13 +1,14 @@
 import styled, { css } from 'styled-components';
 import { BaseProps, BaseView } from '../view';
+import { getSpacing } from '../view/view';
 
 interface Props extends BaseProps {
   shadow?: boolean;
 }
 
 export const Card = styled(BaseView)<Props>(
-  ({ theme, shadow }) => css`
-    padding: ${theme.spacing.md}px;
+  ({ theme, shadow, padding }) => css`
+    padding: ${padding ? getSpacing(theme, padding) : theme.spacing.md + 'px'};
     border-radius: ${theme.spacing.xs}px;
     background: white;
     box-shadow: ${shadow ? theme.shadow.lg : 'none'};
